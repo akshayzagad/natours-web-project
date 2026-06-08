449f7f6ef36803c9118b6a28b853afa815af6ca3
akshayzagad <akshayzagade62@gmail.com.com>
2026-06-08 22:57:58 +0530

    feat: add delete user functionality and active status to user model

--- controllers/userController.js
+++ controllers/userController.js
@@ -72,3 +72,11 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.deleteMe = catchAsync(async (req,res,next)=>{
+  await User.findByIdAndUpdate(req.user.id,{active:false});
+  res.status(204).json({
+    status:'succses',
+    data:null
+  })
+})
--- models/userModel.js
+++ models/userModel.js
@@ -48,6 +48,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active:{
+    type:Boolean,
+    default:true,
+    select:false
+  }
 });
 
 userSchema.pre("save", async function () {
@@ -73,6 +78,11 @@ userSchema.pre('save', function(next) {
   this.passwordChangedAt = Date.now() - 1000;
   
 });
+
+userSchema.pre(/^find/,function(next){
+//This point to current query
+this.find({active: {$ne:false}})
+})
 
 userSchema.methods.correctPassword = async function (
   candidatePassword,
