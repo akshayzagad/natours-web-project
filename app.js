dff5ca6c6296c2c4010f2ecddf8b6bfbfb000451
akshayzagad <akshayzagade62@gmail.com.com>
2026-06-09 14:26:22 +0530

    feat: add data sanitization middleware to prevent NoSQL injection and XSS attacks

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize")
+const xss = require('xss-clean')
 
 const AppError = require("./utils/appError");
 const tourRouter = require("./routes/toursRoutes");
 const userRouter = require("./routes/usersRoutes");
-const morgan = require("morgan");
-const rateLimit = require("express-rate-limit");
-const helmet = require("helmet");
 
 const globalErrorHandler = require("./controllers/errorController");
 
@@ -29,6 +31,12 @@ app.use("/api", limiter);
 
 // Body parser,reading data from body into req.body
 app.use(express.json({limit:'10kb'}));
+
+//Data santization against noSQL query injection
+app.use(mongoSanitize());
+
+//Data santization against cross side scripting attack
+app.use(xss());
 
 // Serving static file page
 app.use(express.static(`${__dirname}/public`));
