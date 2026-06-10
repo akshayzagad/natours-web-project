2057e5e5f59f11423b7f67ab90417512bcc7e71a
akshayzagad <akshayzagade62@gmail.com.com>
2026-06-10 17:27:46 +0530

    feat: enhance review functionality by allowing nested routes and linking user to reviews so uset tourroutes in which bind revivew controller post method and send tour id to write tour revivew  on that specific tour

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -12,6 +12,13 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
+    console.log("req.user =", req.user);
+console.log("req.user.id =", req.user.id);
+console.log("req.body =", req.body);
+  //Allow Nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+
   const newReview = await Review.create(req.body);
 
   res.status(201).json({
--- routes/toursRoutes.js
+++ routes/toursRoutes.js
@@ -1,5 +1,6 @@
 const tourController = require("../controllers/tourcontroller");
-const authcontroller = require("../controllers/authController")
+const authcontroller = require("../controllers/authController");
+const reviewController = require("../controllers/reviewController");
 // Middleware
 const express = require("express");
 
@@ -17,13 +18,29 @@ router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
 
 router
   .route("/")
-  .get(authcontroller.protect,tourController.getAllTours)
+  .get(authcontroller.protect, tourController.getAllTours)
   .post(tourController.createTour);
 
 router
   .route("/:id")
   .get(tourController.getTour)
   .patch(tourController.updateTour)
-  .delete(authcontroller.protect, authcontroller.restrictTo('admin','lead-guide'), tourController.deleteTour);
+  .delete(
+    authcontroller.protect,
+    authcontroller.restrictTo("admin", "lead-guide"),
+    tourController.deleteTour,
+  );
+
+//POST/tour/:tourid/reviews
+//Get/tour/:tourid/reviews
+//POST/tour/:tourid/reviews/id
+
+router
+  .route("/:tourId/reviews")
+  .post(
+    authcontroller.protect,
+    authcontroller.restrictTo("user"),
+    reviewController.createReview,
+  );
 
 module.exports = router;
