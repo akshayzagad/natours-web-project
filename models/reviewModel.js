6d290e48d4e7ea966cc1e04218da91b084302ba8
akshayzagad <akshayzagade62@gmail.com.com>
2026-06-12 17:47:24 +0530

    feat: add calculateAverageRatings method and update reviewSchema to handle average rating calculations

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,6 @@
 const { default: mongoose } = require("mongoose");
 const moongoose = require("mongoose");
+const Tour = require('./tourModel')
 
 const reviewSchema = new moongoose.Schema(
   {
@@ -31,8 +32,8 @@ const reviewSchema = new moongoose.Schema(
   },
 );
 
-reviewSchema.pre(/^find/,function(){
-    // this.populate({
+reviewSchema.pre(/^find/, function () {
+  // this.populate({
   //   path: 'tour',
   //   select: 'name'
   // }).populate({
@@ -41,9 +42,34 @@ reviewSchema.pre(/^find/,function(){
   // });
 
   this.populate({
-    path: 'user',
-    select: 'name photo'
+    path: "user",
+    select: "name photo",
   });
+});
+
+reviewSchema.statics.calculateAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    { $match: { tour: tourId } },
+    {
+      $group: {
+        _id: "$tour",
+        nRating: { $sum:1 },
+        avgRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+  console.log(stats); 
+  await Tour.findByIdAndUpdate(tourId,{
+    ratingsQuantity:stats[0].nRating,
+    ratingsAverage:stats[0].avgRating
+  })
+};
+
+// This middleware write beacause we want to call calculateAverageRatings method
+reviewSchema.post('save',async function(){
+  /** Here we did not use 'Review' because we declare it on line no 68 so we use constructor 
+   * to call above statics method */
+  await this.constructor.calculateAverageRatings(this.tour);
 })
 
 const Review = mongoose.model("Review", reviewSchema);
