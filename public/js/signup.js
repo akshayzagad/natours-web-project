704de0c6d5c2f7cad5b8a9de41fa3fc5d40ced70
akshayzagad <akshayzagade62@gmail.com.com>
2026-06-23 18:54:31 +0530

    Refactor API calls in JavaScript files to use relative URLs and improve code consistency
    - Updated login.js to use relative URL for login API and standardized string quotes.
    - Refactored review.js to use relative URLs for review-related API calls and improved string formatting.
    - Modified signup.js to utilize relative URL for signup API and ensured consistent string quotes.
    - Adjusted updateSetting.js to implement relative URLs for user settings updates and standardized string usage.

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,28 +1,27 @@
-import axios from 'axios';
-import { showAlert } from './alert';
+import axios from "axios";
+import { showAlert } from "./alert";
 
-export const signup = async (name,email, password,passwordConfirm) => {
+export const signup = async (name, email, password, passwordConfirm) => {
   try {
     const res = await axios({
-      method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/signUp',
+      method: "POST",
+      url: "/api/v1/users/signUp",
       data: {
         name,
         email,
         password,
-        passwordConfirm
-      }
+        passwordConfirm,
+      },
     });
-    
-    
-    if (res.data.status === 'success') {
+
+    if (res.data.status === "success") {
       // console.log(res.data.status);
-      showAlert('success', 'Sign up successfully!');
+      showAlert("success", "Sign up successfully!");
       window.setTimeout(() => {
-        location.assign('/');
+        location.assign("/");
       }, 1500);
     }
   } catch (err) {
-    showAlert('error', err.response.data.message);
+    showAlert("error", err.response.data.message);
   }
 };
