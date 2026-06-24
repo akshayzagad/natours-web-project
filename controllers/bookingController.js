c8cf8d0622c2cb1865cb15d55c55b73366f15cad
akshayzagad <akshayzagade62@gmail.com.com>
2026-06-24 19:48:30 +0530

    event log

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -63,7 +63,7 @@ exports.webhookCheckout = async (req, res) => {
   console.log('Webhook hit!');
 
   console.log(req.body);
-
+  console.log(event);
   res.status(200).json({
     status: 'success'
   });
