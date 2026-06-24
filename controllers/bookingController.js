e7650d504a5469c4b7cc18742f7cf4d8dd2a51fb
akshayzagad <akshayzagade62@gmail.com.com>
2026-06-24 20:03:12 +0530

    Refactor bookingController to improve Paystack integration and enhance webhook handling

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,13 +1,12 @@
-const axios = require('axios');
-const paystack = require('@paystack/paystack-sdk');
-
-const Tour = require('../models/tourModel');
-const User = require('../models/userModel');
-const Booking = require('../models/bookingModel')
+const axios = require("axios");
+const paystack = require("@paystack/paystack-sdk");
+const crypto = require('crypto');
+const Tour = require("../models/tourModel");
+const User = require("../models/userModel");
+const Booking = require("../models/bookingModel");
 // const Booking = require('../models/bookingModel');
-const catchAsync = require('../utils/catchAsync');
-const factory = require('./handlerFactory');
-
+const catchAsync = require("../utils/catchAsync");
+const factory = require("./handlerFactory");
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get tour
@@ -15,56 +14,73 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
   // 2) Initialize Paystack transaction
   const response = await axios.post(
-    'https://api.paystack.co/transaction/initialize',
+    "https://api.paystack.co/transaction/initialize",
     {
       email: req.user.email,
 
       // Paystack expects smallest currency unit
       amount: tour.price * 100,
 
-      callback_url: `${req.protocol}://${req.get(
-        'host'
-      )}/?tour=${
-      req.params.tourId
-    }&user=${req.user.id}&price=${tour.price}`,
+      callback_url: `${req.protocol}://${req.get("host")}/my-tours`,
 
       metadata: {
-        tourId: req.params.tourId,
+        tourId: tour.id,
         userId: req.user.id,
-        tourName: tour.name
-      }
+        tourName: tour.name,
+      },
     },
     {
       headers: {
         Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
-        'Content-Type': 'application/json'
-      }
-    }
+        "Content-Type": "application/json",
+      },
+    },
   );
 
   res.status(200).json({
-    status: 'success',
-    session: response.data.data
+    status: "success",
+    session: response.data.data,
   });
-});
-
-exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-  // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
-  const { tour, user, price } = req.query;
-
-  if (!tour && !user && !price) return next();
-  
-  await Booking.create({ tour, user, price });
-
-  res.redirect(req.originalUrl.split('?')[0]);
 });
 
 exports.webhookCheckout = async (req, res) => {
   console.log('Webhook hit!');
 
-  console.log(req.body);
+  // 1. Verify Paystack Signature
+  const hash = crypto
+    .createHmac('sha512', process.env.PAYSTACK_SECRET_KEY)
+    .update(req.body)
+    .digest('hex');
+
+  if (hash !== req.headers['x-paystack-signature']) {
+    return res.status(400).send('Invalid signature');
+  }
+
+  // 2. Parse Event
   const event = JSON.parse(req.body.toString());
-  console.log(event);
+
+  console.log(event.data.metadata);
+
+  // 3. Process Successful Payment
+  if (event.event === 'charge.success') {
+    const metadata = event.data.metadata;
+
+    const existingBooking = await Booking.findOne({
+      tour: metadata.tourId,
+      user: metadata.userId
+    });
+
+    if (!existingBooking) {
+      await Booking.create({
+        tour: metadata.tourId,
+        user: metadata.userId,
+        price: event.data.amount / 100
+      });
+
+      console.log('Booking created');
+    }
+  }
+
   res.status(200).json({
     status: 'success'
   });
