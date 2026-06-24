3893ef69d4222544eff7d921ed531916b7bcd6ca
akshayzagad <akshayzagade62@gmail.com.com>
2026-06-24 19:12:26 +0530

    rate limit error

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
-app.enable('trust proxy');
+app.set('trust proxy', 1);
 
 app.use(cors());
 // Access-Control-Allow-Origin *
